6259ed13d74339e0558307119c129cd709751eb9
Colin Stevens <colinstevens06@gmail.com>
2019-11-18 20:24:14 -0500

    progress on the ajax calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ var currentCitySearch = "";
 
 var cityUserInput = $("#city-input");
 var cityInputButton = $("#city-input-button");
+var cityTodaysDate = $("#todays-date")
+var cityTodaysTemperature = $("#todays-temperature")
+var cityTodaysWindSpeed = $("#todays-wind-speed")
+var cityTodaysUvIndex = $("#todays-uv-index")
 
-console.log(cityUserInput)
 
 
 
@@ -15,24 +18,64 @@ function getPreviousCities() {
 };
 
 function storeData(event) {
-   event.preventDefault()
-   console.log(cityUserInput)
+   event.preventDefault();
 
-   var userInput = cityUserInput.value;
+   var userInput = cityUserInput.val();
 
-   console.log(userInput)
+   currentCitySearch = "";
+   currentCitySearch = userInput;
 
-   // currentCitySearch = "";
-   // currentCitySearch = userInput;
+   if (userInput === "") {
+      alert("Please put in a valid city")
+      return
+   };
 
-   // if (userInput === "") {
-   //    alert("Please put in a valid city")
-   // }
+   previousCitySearches.push(userInput);
 
-   // previousCitySearches.push(userInput);
+   localStorage.setItem("city", JSON.stringify(previousCitySearches))
 
-   // localStorage.setItem("city", JSON.stringify(userInput))
+};
+
+function presentTodaysWeatherData(event) {
+   event.preventDefault();
+
+   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+   var uviURL = "https://"
+   var userInput = cityUserInput.val();
+   var apiKey = "&appid=3c98be5119ec5cf431d72d940860a3bc";
+   var queryURL = weatherURL + userInput + apiKey;
+   var uviURL = weatherURL
+   var lat = "";
+   var long = "";
+
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+   }).then(
+      function(response) {
+         cityTodaysDate.text("NEED TO FIGURE THIS ONE OUT")
+
+         // converting temperature from kelvin to fahrenheit
+         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+         cityTodaysTemperature.text("Temperature: " + tempF)
+         cityTodaysWindSpeed.text("Wind speed: " + response.wind.speed)
+         cityTodaysUvIndex.text("UV Index: " + response.)
+
+
+
+      }
+
+
+
+   )
+
+
+
+
+
 
 };
 
 cityInputButton.on("click", storeData);
+cityInputButton.on("click", presentTodaysWeatherData);
